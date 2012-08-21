75ff4889b97e7b24f0254f8c91acbf99aba23d18
Jasper Van der Jeugt <m@jaspervdj.be>
2012-08-21 13:29:16 +0900

    parsergen: Draft generic repack

--- ParserGen/Repack.hs
+++ ParserGen/Repack.hs
@@ -1,185 +1,128 @@
-{-# LANGUAGE TemplateHaskell, QuasiQuotes, RecordWildCards #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 module ParserGen.Repack
-    ( genRepackFromFile
-    , putDecimalX
-    , putDecimalXS
-    , putTS8
+    ( RepackConstructor
+    , repackConstructor
+
+    , RepackField
+    , repackField
+
+    , genRepackFromFile
     ) where
+
+import Control.Applicative
+import Control.Monad (foldM, guard)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import Data.Maybe (fromMaybe, listToMaybe, mapMaybe)
+import Language.Haskell.TH as TH
+import Language.Haskell.TH.Syntax (Lift)
+import Unsafe.Coerce (unsafeCoerce)
 
 import ParserGen.ParseQuote
 
-import Control.Applicative
-import Control.Monad (guard, foldM)
-import Data.Maybe (mapMaybe)
-import Language.Haskell.TH as TH
-import qualified Data.ByteString.Char8 as C8
+import Debug.Trace
 
-
-data ToRepack
-    = ToRepack
-    { trPacketName :: String -- to generate name repackerForXXXXXXX
-    , trPrefix     :: String -- to validate correctness of the packet
-    , trPacketSize :: Int    -- to validate correctness of the packet
-    , trPieces     :: [PackElement]
+data RepackConstructor = RepackConstructor
+    { repackConstrName   :: String
+    , repackConstrSuffix :: String
+    , repackConstrFields :: [RepackField]
     } deriving (Show)
 
-data PackElement
-    = PackBS Int -- leave this number of bytes as it is
-    | PackBids Int String
-    | PackAsks Int String
---    | PackIssue
---    | PackTS
+repackConstructor :: String -> String -> [RepackField] -> RepackConstructor
+repackConstructor = RepackConstructor
+
+data RepackField = RepackField
+    { repackFieldName      :: String
+    , repackFieldTransform :: ExpQ
+    }
+
+instance Show RepackField where
+    show (RepackField name _) = "(RepackField " ++ show name ++ " _)"
+
+repackField :: String -> ExpQ -> RepackField
+repackField name exp = RepackField name exp
+
+class Repackable a where
+    repack :: a -> [ByteString]  -- TODO blaze builder
+
+instance Repackable a => Repackable [a] where
+    repack = concat . map repack
+    {-# INLINE repack #-}
+
+instance Repackable ByteString where
+    repack = return
+    {-# INLINE repack #-}
+
+genRepackFromFile :: FilePath -> [RepackConstructor] -> Q [Dec]
+genRepackFromFile templateName rcs = do
+    dts <- getDatatypes templateName
+    fmap concat $ mapM (mkRepacker dts) rcs
+
+mkRepacker :: [Datatype] -> RepackConstructor -> Q [Dec]
+mkRepacker dts (RepackConstructor cname csuffix cfields) = do
+    withNames <- mapM (\cf -> (,) cf <$> newName "p") cfields
+    let repackCmds = mkRepackCmds dc withNames
+
+    bsVar    <- newName "bs"
+    undef    <- [|undefined|]
+    btbT     <- [t|ByteString -> ByteString|]
+    foldInit <- [|($(return $ VarE bsVar) , [])|]
+    fold     <- foldM executeRepackCmd foldInit repackCmds
+    body     <- [|B.concat $ snd $(return fold)|]
+
+    return
+        [ SigD repackerName (foldr mkType btbT cfields)
+        , FunD repackerName
+            [ Clause (map (VarP . snd) withNames ++ [VarP bsVar])
+              (NormalB body) []
+            ]
+        ]
+  where
+    repackerName = mkName $ "repackerFor" ++ cname ++ csuffix
+
+    dc = case [c | dt <- dts, c <- typeConstrs dt, constrName c == cname] of
+        [x] -> x
+        _   -> error $ "No genparser for constructor " ++ cname
+
+    mkType (RepackField name _) t =
+        AppT (AppT ArrowT (getFieldType name dc)) t
+
+getFieldType :: String -> DataConstructor -> Type
+getFieldType n dc =
+    case [fieldType f | f <- constrFields dc, fieldName f == Just n] of
+        [t] -> t
+        _   -> error $ constrName dc ++ " has no field " ++ n
+
+data RepackCmd
+    = Skip Int
+    | Repack Int RepackField Name
     deriving (Show)
 
+fuseSkips :: [RepackCmd] -> [RepackCmd]
+fuseSkips (Skip a : Skip b : rcs) = fuseSkips $ Skip (a + b) : rcs
+fuseSkips (r      : rcs)          = r : fuseSkips rcs
+fuseSkips []                      = []
 
+mkRepackCmds :: DataConstructor -> [(RepackField, Name)] -> [RepackCmd]
+mkRepackCmds dc repacks = fuseSkips $ map mkRepackCmd $ constrFields dc
+  where
+    mkRepackCmd :: DataField -> RepackCmd
+    mkRepackCmd df = fromMaybe (Skip $ getFieldWidth df) $ listToMaybe
+        [ Repack (getFieldWidth df) rf n
+        | (rf, n) <- repacks
+        , fieldName df == Just (repackFieldName rf)
+        ]
 
-putDecimalX :: Int -> Int -> C8.ByteString
-putDecimalX l i = C8.pack $ putDecimalX_S l i
-
-putDecimalXS :: Int ->  Int -> C8.ByteString
-putDecimalXS l i | i >= 0 = C8.pack $ ' ' : putDecimalX_S l i
-putDecimalXS l i | otherwise = C8.pack $ '-' : putDecimalX_S l (negate i)
-
-putTS8 :: Int -> Int -> Int -> Int -> C8.ByteString
-putTS8 h m s u = C8.pack $ concat [ putDecimalX_S 2 h
-                                  , putDecimalX_S 2 m
-                                  , putDecimalX_S 2 s
-                                  , putDecimalX_S 2 u
-                                  ]
--- helper function
-putDecimalX_S :: Int -> Int -> String
-putDecimalX_S l i | i >= 0 = reverse . take l . reverse $ (replicate l '0' ++ show i)
-putDecimalX_S _ i = error "ParserGen.Repack: Can't put negative decimal X: " ++ show i
-
-
-
-genRepackFromFile :: FilePath -> Q [Dec]
-genRepackFromFile templateName =
-    getDatatypes templateName >>= fmap concat . mapM mkModifiers
-
-
-mkModifiers :: Datatype -> Q [Dec]
-mkModifiers dt = mapM mkRepacker (getRepack dt) >>= return . concat
-
-
-mkRepacker :: ToRepack -> Q [Dec]
-mkRepacker (ToRepack {..}) = do
-        -- validation function - string length and prefix should match
-        val <- [| \b -> trPacketSize == C8.length b && (C8.pack trPrefix) `C8.isPrefixOf` b |]
-
-        -- type signature for declared functions
-        sig <- [t| C8.ByteString -> [(Int, Int)] -> [(Int, Int)] -> C8.ByteString |]
-        foldInit <- [| ( $(return $ VarE pkt) , []) |]
-
-        -- after some magical process we produce the actual bytestring result
-        -- C8.concat should be replaced with blazebuilder
-        bs <- [| C8.concat $ snd $(foldM processPiece foldInit trPieces) |]
-
-        let modify = (NormalG $ AppE val (VarE pkt), bs)
-            keep = (NormalG $ VarE 'otherwise, VarE pkt)
-
-        let fun = [Clause pat (GuardedB [modify, keep]) []]
-        return [SigD name sig, FunD name fun]
-    where
-
-        processPiece :: Exp -> PackElement -> Q Exp-- {{{
-        processPiece e (PackBS l) =
-            [| let (s, ps)      = $(return e)
-                   (this, next) = C8.splitAt l s
-               in (next, ps ++ [this]) |]
-
-        processPiece e (PackBids i f) = do
-            [| let (s, ps) = $(return e)
-                   next = C8.drop i s
-               in (next, ps ++ [ $(return . VarE . mkName $ f) $(return $ VarE bids) ] ) |]
-
-        processPiece e (PackAsks i f) = do
-            [| let (s, ps) = $(return e)
-                   next = C8.drop i s
-               in (next, ps ++ [ $(return . VarE . mkName $ f) $(return $ VarE asks) ] ) |]
-        {-
-        processPiece e PackTS = do
-            [| let (s, ps) = $(return e)
-                   next = C8.drop 8 s
-               in (next, ps ++ [ $(return $ VarE ts) ]) |]-- }}}
-
-        processPiece e PackIssue = do
-            [| let (s, ps) = $(return e)
-                   next = C8.drop 12 s
-                in (next, ps ++ [ $(return $ VarE issue) ] ) |]
-        -}
-
-
-
-        asks  = mkName "asks"   :: Name
-        bids  = mkName "bids"   :: Name
-        pkt   = mkName "pkt"    :: Name
-        --issue = mkName "issue"  :: Name
-        --ts    = mkName "ts"     :: Name
-
-        pat :: [Pat]
-        --pat = [ VarP pkt, VarP issue, VarP asks, VarP bids, VarP ts ]
-        pat = [ VarP pkt, VarP asks, VarP bids ]
-
-
-        name = mkName $ "repackerFor" ++ trPacketName :: Name
-
-        -- 1) validate base packet if it requires any modifications at all
-        -- 2) split packet into [ByteString], modifying PackIssue/PackAsks/PackBids/PackTS
-        -- 3) join them back with blaze builder
-
-
-getRepack :: Datatype -> [ToRepack]
-getRepack =  mapMaybe maybeRepack . typeConstrs
-    where
-
-        maybeRepack :: DataConstructor -> Maybe ToRepack
-        maybeRepack dc = do
-            let fs = constrFields dc
-                trPacketName = constrName dc
-                trPacketSize = sum . map getFieldWidth $ fs
-            (DataField Nothing Nothing _ _ _ (HardcodedString trPrefix): _) <- Just fs
-            trPieces <- fusePackBS <$> mapM toPiece fs
-            guard $ and [ any isAsks fs
-                        , any isBids fs
-                        -- , any isIssueCode fs
-                        -- , any isTS fs
-                        ]
-            return ToRepack {..}
-
-        fusePackBS :: [PackElement] -> [PackElement]
-        fusePackBS (PackBS a : PackBS b : ps) = fusePackBS (PackBS (a+b) : ps)
-        fusePackBS (p:ps) = p : fusePackBS ps
-        fusePackBS [] = []
-
-        toPiece :: DataField -> Maybe PackElement
-        toPiece df = case () of
-            _ | isAsks df -> PackAsks (getFieldWidth df) <$> (("bs_" ++) <$> getCustomParser df)
-              | isBids df -> PackBids (getFieldWidth df) <$> (("bs_" ++) <$> getCustomParser df)
-            --  | isIssueCode df -> Just PackIssue
-            --  | isTS df        -> Just PackTS
-              | otherwise      -> Just $ PackBS (getFieldWidth df)
-
-        getCustomParser :: DataField -> Maybe String
-        getCustomParser df = case fieldParser df of
-                                CustomParser (VarE n) -> Just $ nameBase n
-                                _ -> Nothing
-
-        isAsks :: DataField -> Bool
-        isAsks (DataField (Just "BestAsks") (Just 5) _ _ _ (CustomParser _)) = True
-        isAsks _ = False
-
-        isBids :: DataField -> Bool
-        isBids (DataField (Just "BestBids") (Just 5) _ _ _ (CustomParser _)) = True
-        isBids _ = False
-
-        {-
-        isTS :: DataField -> Bool
-        isTS f = case getCustomParser f of
-                    Just "ts8" -> True
-                    _ -> False
-
-        isIssueCode :: DataField -> Bool
-        isIssueCode (DataField (Just "IssueCode") _ _ _ _ _) = True
-        isIssueCode _ = False
-        -}
+executeRepackCmd :: Exp -> RepackCmd -> Q Exp
+executeRepackCmd exp (Skip n) =
+    [| let (s, ps)      = $(return exp)
+           (this, next) = B.splitAt n s
+       in (next, ps ++ [this]) |]
+executeRepackCmd exp (Repack n (RepackField _ f) name) =
+    [| let (s, ps)      = $(return exp)
+           (this, next) = B.splitAt n s
+       in (next, ps ++ repack ($(f) $(return $ VarE name))) |]
