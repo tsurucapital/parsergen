aca5c899b9c73df441ffc66e3181be23ef547c55
Jasper Van der Jeugt <m@jaspervdj.be>
2012-08-22 10:39:11 +0900

    parsergen: Add benchmarks file

---
+++ benchmarks/Main.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Control.DeepSeq (NFData)
+import Criterion (Pure, bench, nf)
+import Criterion.Main (defaultMain)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+
+import ParserGen.Parser
+
+main :: IO ()
+main = defaultMain
+    [ bench "unsafeDecimalX" $ benchParse (unsafeDecimalX 10) input
+    ]
+
+benchParse :: NFData a => Parser a -> ByteString -> Pure
+benchParse parser bs = nf (parse parser) bs
+
+input :: ByteString
+input = B.concat $ replicate 100 "1234"
