825dc211f24362dd3fd006df36259aa807aae8d2
John Lato <jwlato@tsurucapital.com>
2013-08-29 14:22:14 +0900

    unpack bytestrings in parser

--- src/ParserGen/Parser.hs
+++ src/ParserGen/Parser.hs
@@ -6,32 +6,33 @@
 -- unsafe versions of the functions do not perform checks that there
 -- is enough data left in the bytestring
 {-# LANGUAGE BangPatterns, MagicHash, UnboxedTuples #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -ddump-simpl -ddump-to-file -ddump-asm #-}
 module ParserGen.Parser
     ( Parser
     , parse
+    , consumeWord8
+    , unsafeTake
+    , unsafeSkip
     , ensureBytesLeft
-    , atEnd
     , string
     , anyChar
     , take
-    , unsafeTake
     , skip
-    , unsafeSkip
-    , takeWhile
     ) where
 
 import Data.Word (Word8)
 import Control.Applicative
 import Control.Monad
+import Foreign.ForeignPtr
+import Foreign.Ptr
+import Foreign.Storable (peekByteOff)
 import Data.Monoid
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Unsafe as B
+import Data.ByteString.Internal (ByteString(..), inlinePerformIO)
 import Data.ByteString (ByteString)
 import Prelude hiding (take, takeWhile)
-
-newtype S = S {
-      input :: ByteString
-    }
 
 data Result a = Fail String
               | OK !a
@@ -42,58 +43,28 @@ data Result a = Fail String
 -- ('>>=') evaluates each result to weak head normal form before
 -- passing it along.
 newtype Parser a = Parser {
-      runParser :: S -> (# Result a, S #)
+      runParser :: ForeignPtr Word8 -- payload
+                -> Int              -- offset
+                -> Int              -- length
+                -> (# Result a, ForeignPtr Word8, Int, Int #)
     }
 
 instance Functor Parser where
-    fmap f m = Parser $ \s -> case runParser m s of
-                                (# OK a, s' #)     -> (# OK (f a), s' #)
-                                (# Fail err, s' #) -> (# Fail err, s' #)
+    fmap g m = Parser $ \f o l -> case runParser m f o l of
+                (# OK a, f', o', l' #)     -> (# OK (g a), f', o', l' #)
+                (# Fail err, f', o', l' #) -> (# Fail err, f', o', l' #)
     {-# INLINE fmap #-}
 
 instance Monad Parser where
-    return a = Parser $ \s -> (# OK a, s #)
+    return a = Parser $ \f o l -> (# OK a, f, o, l #)
     {-# INLINE return #-}
 
-    m >>= k   = Parser $ \s -> case runParser m s of
-                                 (# OK a, s' #) -> runParser (k a) s'
-                                 (# Fail err, s' #) -> (# Fail err, s' #)
+    m >>= k   = Parser $ \f o l -> case runParser m f o l  of
+                 (# OK a, f', o', l' #) -> runParser (k a) f' o' l'
+                 (# Fail err, f', o', l' #) -> (# Fail err, f', o', l' #)
     {-# INLINE (>>=) #-}
 
-    fail msg = Parser $ \s -> (# Fail msg, s #)
-
-instance MonadPlus Parser where
-    mzero = fail "mzero"
-    {-# INLINE mzero #-}
-
-    mplus a b = Parser $ \s ->
-                case runParser a s of
-                  (# ok@(OK _), s' #) -> (# ok, s' #)
-                  (# _, _ #) -> case runParser b s of
-                                   (# ok@(OK _), s'' #) -> (# ok, s'' #)
-                                   (# err, s'' #) -> (# err, s'' #)
-    {-# INLINE mplus #-}
-
-instance Applicative Parser where
-    pure   = return
-    {-# INLINE pure #-}
-    (<*>)  = ap
-    {-# INLINE (<*>) #-}
-
-gets :: (S -> a) -> Parser a
-gets f = Parser $ \s -> (# OK (f s), s #)
-{-# INLINE gets #-}
-
-put :: S -> Parser ()
-put s = Parser $ \_ -> (# OK (), s #)
-{-# INLINE put #-}
-
--- | Run a parser.
-parse :: Parser a -> ByteString -> Either String a
-parse p bs = case runParser p (S bs) of
-    (# OK a, _ #)     -> Right a
-    (# Fail err, _ #) -> Left err
-{-# INLINE parse #-}
+    fail msg = Parser $ \f o l -> (# Fail msg, f, o, l #)
 
 instance Monoid (Parser a) where
     mempty  = fail "mempty"
@@ -101,81 +72,106 @@ instance Monoid (Parser a) where
     mappend = mplus
     {-# INLINE mappend #-}
 
+instance MonadPlus Parser where
+    mzero = fail "mzero"
+    {-# INLINE mzero #-}
+
+    mplus a b = Parser $ \f o l ->
+                case runParser a f o l of
+                  (# ok@OK{}, f', o', l' #) -> (# ok, f', o', l' #)
+                  (# _, _, _, _ #) -> case runParser b f o l of
+                                   (# ok@OK{}, f', o', l' #) -> (# ok, f', o', l' #)
+                                   (# err, f', o', l' #) -> (# err, f', o', l' #)
+    {-# INLINE mplus #-}
+
 instance Alternative Parser where
     empty = fail "empty"
     {-# INLINE empty #-}
     (<|>) = mplus
     {-# INLINE (<|>) #-}
 
--- | Consume input while the predicate returns 'True'.
-takeWhile :: (Word8 -> Bool) -> Parser ByteString
-takeWhile p = do
-  (h,t) <- gets (B.span p . input)
-  put (S t)
-  return h
-{-# INLINE takeWhile #-}
+instance Applicative Parser where
+    pure   = return
+    {-# INLINE pure #-}
+    (<*>)  = ap
+    {-# INLINE (<*>) #-}
+
+-- | Run a parser.
+parse :: Parser a -> ByteString -> Either String a
+parse p (PS fptr offset availSize) = case runParser p fptr offset availSize of
+        (# OK a, _, _, _ #)     -> Right a
+        (# Fail err, _, _, _ #) -> Left err
+{-# INLINE parse #-}
+
+{-# INLINE consumeWord8 #-}
+consumeWord8 :: Parser Word8
+consumeWord8 = Parser $ \f o l ->
+    let w8 = inlinePerformIO $ withForeignPtr f $ \p -> peekByteOff p o
+    in (# OK w8, f, o+1, l-1 #)
 
 -- | Consume @n@ bytes of input.
 take :: Int -> Parser ByteString
 take !n = do
-  s <- gets input
-  if B.length s >= n
-    then put (S (B.unsafeDrop n s)) >> return (B.unsafeTake n s)
-    else fail "insufficient input for take"
+    !l <- getsLength
+    if l >= n
+        then unsafeTake n
+        else fail "insufficient input for take"
 {-# INLINE take #-}
 
+
+getsPtr :: Parser (ForeignPtr Word8)
+getsPtr = Parser $ \f o l -> (# OK f, f, o, l #)
+
+getsOffset :: Parser Int
+getsOffset = Parser $ \f o l -> (# OK o, f, o, l #)
+
+getsLength :: Parser Int
+getsLength = Parser $ \f o l -> (# OK l, f, o, l #)
+
 ensureBytesLeft :: Int -> Parser ()
-ensureBytesLeft l = do
-    s <- gets input
-    if B.length s == l
+ensureBytesLeft ex = do
+    l <- getsLength
+    if ex == l
         then return ()
-        else fail $ "Unexpected length: expected " ++ show l ++
-                ", but got " ++ show (B.length s)
+        else fail $ "Unexpected length: expected " ++ show ex ++
+                ", but got " ++ show l
 {-# INLINE ensureBytesLeft #-}
 
 -- | Consume @n@ bytes of input without checking if it's available
 unsafeTake :: Int -> Parser ByteString
-unsafeTake !n = do
-    s <- gets input
-    put (S (B.unsafeDrop n s))
-    return (B.unsafeTake n s)
+unsafeTake !n = Parser $ \f o l ->
+    let bs = PS f o n
+    in (# OK bs, f, o + n, l - n #)
 {-# INLINE unsafeTake #-}
 
 -- | Skip @n@ bytes of input
 skip :: Int -> Parser ()
 skip !n = do
-    s <- gets input
-    if B.length s >= n
-        then put (S (B.unsafeDrop n s))
+    l <- getsLength
+    if l >= n
+        then unsafeSkip n
         else fail "insufficient input for skip"
 {-# INLINE skip #-}
 
 -- | Skip @n@ bytes of input without checking if it's available
 unsafeSkip :: Int -> Parser ()
-unsafeSkip !n = gets input >>= put . S . B.unsafeDrop n
+unsafeSkip !n = Parser $ \f o l -> (# OK (), f, o + n, l - n #)
 {-# INLINE unsafeSkip #-}
+
 
 -- | Match a string exactly.
 string :: ByteString -> Parser ()
-string s = do
-  i <- gets input
-  if s `B.isPrefixOf` i
-    then put (S (B.unsafeDrop (B.length s) i))
-    else fail $ "string"
+string s@(PS _ _ l) = Parser $ \fptr offset avail ->
+    if s == PS fptr offset (min l avail)
+        then (# OK (), fptr, offset + l, avail - l #)
+        else (# Fail ("string:"), fptr, offset, avail #)
 {-# INLINE string #-}
 
 anyChar :: Parser Char
 anyChar = do
-    s <- gets input
-    put (S (B.unsafeDrop 1 s))
-    return (w2c $! B.unsafeHead s)
-
--- | Indicate whether the end of the input has been reached.
-atEnd :: Parser Bool
-atEnd = do
-  i <- gets input
-  return $! B.null i
-{-# INLINE atEnd #-}
+    s <- consumeWord8
+    return (w2c $! s)
+{-# INLINE anyChar #-}
 
 w2c :: Word8 -> Char
 w2c = toEnum . fromEnum
